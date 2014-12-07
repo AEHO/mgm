1c63e23275a289e29c459ca7efcea5e9f5e963da
Ciro S. Costa <ciro.costa@usp.br>
2014-12-07 18:19:10 -0200

    updates w/ remote upload

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -5,13 +5,33 @@ var React = require('react');
 var VideoTabs = require('./index/VideoTabs.jsx');
 var VideoAddModal = require('./VideoAddModal.jsx');
 var Modal = require('./Modal.jsx');
-var {AppStore} = require('../stores');
+var {AppStore, PadStore} = require('../stores');
 var {storesGlueMixin} = require('../mixins');
 
 var Index = React.createClass({
 	mixins: [storesGlueMixin(AppStore)],
 
 	getStateFromStores: AppStore.getAppState,
+
+  handleFileChange (e) {
+    var file = e.target.files[0];
+
+    var fd = new FormData();
+    fd.append('name', file.name);
+    fd.append('contents', file);
+
+    var request = new XMLHttpRequest();
+    request.open("POST", '/upload/mobile', true);
+    request.onload = (oEvent) => {
+      console.log(request.responseText);
+    };
+
+    request.send(fd);
+  },
+
+  handleFileSelectClick () {
+    this.refs.fileInp.getDOMNode().click();
+  },
 
 	render () {
 		var modal = this.state.modal.showing ?
@@ -26,9 +46,13 @@ var Index = React.createClass({
       </main> :
       <main>
         <h1 className="main-title">Vixer</h1>
-        <button>upload</button>
+        <div>
+          <button className="fileSelect" onClick={this.handleFileSelectClick}>Select Files</button>
+          <input className="fileInp" ref="fileInp" type="file"
+                 accept="video/*" multiple onChange={this.handleFileChange} />
+        </div>
         <button>play</button>
-      </main>
+      </main>;
 
 		return (
       main
