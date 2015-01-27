6f96a2a2d33ed3ed116152c2118d19e1060d1804
Ciro S. Costa <ciro.costa@usp.br>
2015-01-27 13:59:21 -0200

    adds drag handlers w/ no regressions

--- src/components/AudioGraph.jsx
+++ src/components/AudioGraph.jsx
@@ -19,17 +19,24 @@ const AudioGraph = React.createClass({
 
   handleDragStart (e) {
     e.stopPropagation();
-
-    console.log('hue');
+    AppActions.movingInternalAsset();
   },
 
-  handleDrop (e) {
+  handleDragEnd (e) {
+    AppActions.notMovingInternalAsset();
+  },
+
+  handleInputDrop (e) {
     e.preventDefault();
 
     var state = assign({}, this.state);
     state.input.push(e.dataTransfer.files[0]);
 
     this.setState(state);
+  },
+
+  handleAssetsFolderClick () {
+    AppActions.showSidebar();
   },
 
   render () {
@@ -40,30 +47,46 @@ const AudioGraph = React.createClass({
           <section>
             <h2>input</h2>
             <div>
-              <img onDragStart={this.handleDragStart} className="Item" src="assets/folder.svg" />
-              <img className="Item" src="assets/waves.svg" />
-              <img className="Item" src="assets/soundcloud.svg" />
-              <img className="Item" src="assets/mic.svg" />
+              <img draggable={false}
+                   onClick={this.handleAssetsFolderClick}
+                   className="Item clickable" src="assets/folder.svg" />
+              <img onDragStart={this.handleDragStart}
+                   onDragEnd={this.handleDragEnd}
+                   className="Item" src="assets/waves.svg" />
+              <img onDragStart={this.handleDragStart}
+                   onDragEnd={this.handleDragEnd}
+                   className="Item" src="assets/soundcloud.svg" />
+              <img onDragStart={this.handleDragStart}
+                   onDragEnd={this.handleDragEnd}
+                   className="Item" src="assets/mic.svg" />
             </div>
           </section>
           <section>
             <h2>processing</h2>
             <div>
-              <img className="Item" src="assets/graph.svg" />
-              <img className="Item" src="assets/gain-control.svg" />
-              <img className="Item" src="assets/filter-magic.svg" />
+              <img onDragStart={this.handleDragStart}
+                   onDragEnd={this.handleDragEnd}
+                   className="Item" src="assets/graph.svg" />
+              <img onDragStart={this.handleDragStart}
+                   onDragEnd={this.handleDragEnd}
+                   className="Item" src="assets/gain-control.svg" />
+              <img onDragStart={this.handleDragStart}
+                   onDragEnd={this.handleDragEnd}
+                   className="Item" src="assets/filter-magic.svg" />
             </div>
           </section>
           <section>
             <h2>output</h2>
             <div>
-              <img className="Item" src="assets/speaker.svg" />
+              <img onDragStart={this.handleDragStart}
+                   onDragEnd={this.handleDragEnd}
+                   className="Item" src="assets/speaker.svg" />
             </div>
           </section>
         </aside>
 
         <h1>Audio Graph</h1>
-        <main className="AudioContext container" onDrop={this.handleDrop}>
+        <main className="AudioContext container" onDrop={this.handleInputDrop}>
           <section>
             <p>drop something here</p>
           </section>
